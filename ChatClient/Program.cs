a34c47d50c574273fb583a147fd1c74013f73cd3
Jonas Hansen <jonas.hansen@visma.com>
2022-06-16 12:39:53 +0200

    Made updates

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,43 +1,74 @@
 ﻿using Engine;
-using System;
 using System.Collections.Generic;
 
 namespace ChatClientApp
 {
     internal class Program
     {
+        static string username = null;
+        static string inputStr = "";
+        static List<string> messages = new List<string>();
+
         static void Main(string[] args)
         {
-            List<string> messages = new List<string>();
+            ConsoleEx.Create(64, 16);
+            ConsoleEx.SetFont("Consolas", 16, 32);
 
-            ConsoleEx.Create(128, 32);
-            ConsoleEx.SetFont("Consolas", 8, 16);
-
-            string inputStr = "";
             while (true)
             {
-                foreach(string message in messages)
+                if (username == null)
                 {
-                    ConsoleEx.WriteLine(message);
+                    UsernameView();
                 }
-
-                Draw.Rectangle(0, (int)(ConsoleEx.Height / 1.5f), ConsoleEx.Width, ConsoleEx.Height, false, '.', 0);
-                Draw.Line(0, (int)(ConsoleEx.Height / 1.5f), ConsoleEx.Width, (int)(ConsoleEx.Height / 1.5f), '═');
-                
-                if (Input.KeyPressed(Key.RETURN))
+                else
                 {
-                    messages.Add("User: " + inputStr);
-                    inputStr = "";
+                    ChatView();
                 }
-
-                inputStr = Input.Read(inputStr);
-                ConsoleEx.SetPosition(1, (int)(ConsoleEx.Height / 1.5f) + 2);
-                ConsoleEx.Write("Message: ");
-                ConsoleEx.Write(inputStr);
-                ConsoleEx.Write("|");
 
                 ConsoleEx.Update();
+                ConsoleEx.Clear();
             }
+        }
+
+        static void UsernameView()
+        {
+            inputStr = Input.Read(inputStr);
+            ConsoleEx.SetPosition(ConsoleEx.Width / 3, ConsoleEx.Height / 2);
+            ConsoleEx.Write("Username: ");
+            ConsoleEx.Write(inputStr);
+            ConsoleEx.Write("|");
+
+            if (Input.KeyPressed(Key.RETURN))
+            {
+                username = inputStr;
+                inputStr = "";
+
+                ConsoleEx.Create(128, 32);
+                ConsoleEx.SetFont("Consolas", 8, 16);
+            }
+        }
+
+        static void ChatView()
+        {
+            foreach (string message in messages)
+            {
+                ConsoleEx.WriteLine(message);
+            }
+
+            Draw.Rectangle(0, (int)(ConsoleEx.Height / 1.5f), ConsoleEx.Width, ConsoleEx.Height, false, '.', 0);
+            Draw.Line(0, (int)(ConsoleEx.Height / 1.5f), ConsoleEx.Width, (int)(ConsoleEx.Height / 1.5f), '═');
+
+            if (Input.KeyPressed(Key.RETURN))
+            {
+                messages.Add(username + ": " + inputStr);
+                inputStr = "";
+            }
+
+            inputStr = Input.Read(inputStr);
+            ConsoleEx.SetPosition(1, (int)(ConsoleEx.Height / 1.5f) + 2);
+            ConsoleEx.Write("Message: ");
+            ConsoleEx.Write(inputStr);
+            ConsoleEx.Write("|");
         }
     }
 }
