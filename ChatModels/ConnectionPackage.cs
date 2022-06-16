18019aeb6c44aa8487fa98ac3bc7072b77958128
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2022-06-16 11:35:47 +0200

    Added chat models

---
+++ ChatModels/ConnectionPackage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatModels
+{
+    public class ConnectionPackage
+    {
+        public string Name { get; set; }
+        public string PublicKey { get; set; }
+
+        public ConnectionPackage()
+        {
+
+        }
+        public ConnectionPackage(string name, string publicKey)
+        {
+            Name = name;
+            PublicKey = publicKey;
+        }
+    }
+}
